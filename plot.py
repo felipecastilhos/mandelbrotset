ac4ede17b86db95bee35d6b731dcf572cba5eb7b
Felipe Castilhos <felipecastilhos@gmail.com>
2020-03-10 23:30:47 -0300

    Create the plot function

---
+++ plot.py
@@ -0,0 +1,32 @@
+from PIL import Image, ImageDraw
+from mandelbrot import mandelbrot, MAX_ITER
+
+#Image size (pixels)
+WIDTH = 600
+HEIGHT = 400
+
+#Plot window
+RE_START = -2
+RE_END = 1
+IM_START = -1
+IM_END = 1
+
+palette = []
+
+im = Image.new('RGB', (WIDTH, HEIGHT), (0,0,0))
+draw = ImageDraw.Draw(im)
+
+for x in range(0, WIDTH):
+    for y in range(0, HEIGHT):
+        # Convert pixel cordinate to complex number
+        c = complex(RE_START + (x / WIDTH) * (RE_END - RE_START),
+        IM_START + (y / HEIGHT) * (IM_END - IM_START))
+        
+        # Get total interations
+        m = mandelbrot(c)
+        # Get color based on interations
+        color = 255 - int(m * 255 / MAX_ITER)
+        # Plot the point
+        draw.point([x,y], (color, color, color))
+
+im.save('output.png', )
